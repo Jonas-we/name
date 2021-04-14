8790f739b176cfb4337997ca0df636b07e1d1f28
Jonas-we <81400544+Jonas-we@users.noreply.github.com>
2021-04-14 07:50:57 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,24 @@
 //https://www.eclipse.org/paho/clients/js/
 
-function Historial_1() {
+function Historial_1(message) {
 	
 	console.log("Historial_1");
-	message = new Paho.MQTT.Message("Historial_1");
+	/*message = new Paho.MQTT.Message("Historial_1");
     	message.destinationName = "jhsabel@gmail.com/prueba1";
     	client.send(message);
+	*/
+	document.getElementById("sens1").innerHTML=message.payloadString;
+
+	
   
 }
 function Historial_2(){	
 	console.log("Historial_2");
-	message = new Paho.MQTT.Message("Historial_2");
+	/*message = new Paho.MQTT.Message("Historial_2");
     	message.destinationName = "jhsabel@gmail.com/prueba1";
-    	client.send(message);
+    	client.send(message);*/
+ 	document.getElementById("sens2").innerHTML=message.payloadString;
+
 }
 
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
@@ -32,10 +38,12 @@ function Historial_2(){
   client.connect(options);
    
   // called when the client connects
-  function onConnect() {
+  function onConnect(cont,i,j) {
+int cont=1;
+int i=0;
+
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
-	
     client.subscribe("jhsabel@gmail.com/prueba1");
    message = new Paho.MQTT.Message("Conexion Establecida");
     message.destinationName = "jhsabel@gmail.com/prueba1";
@@ -48,22 +56,25 @@ function Historial_2(){
 	
   }
 
+
+
   // called when the client loses its connection
-  function onConnectionLost(responseObject) {
+  function onConnectionLost(responseObject,cont,i,j) {
     if (responseObject.errorCode !== 0) {
       console.log("onConnectionLost:"+responseObject.errorMessage);
+	    cont=0;
+	    i=0;
+	    
     }
   }
 
   // called when a message arrives
-  function onMessageArrived(message) {
+  function onMessageArrived(cont,message) {
     console.log("onMessageArrived:"+message.payloadString);
 	  mensaje=message.payloadString;
-	  if(mensaje[0]=="1"){
-	 document.getElementById("sens1").innerHTML=mensaje;  
-	  }
-	   if(mensaje[0]=="2"){
-	 document.getElementById("sens2").innerHTML=mensaje;  
-	  }
+	 cont=cont+1;
+	
   }
+
+
   
