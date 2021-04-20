aad11940ceb5ed03b5f08f0862c393d5af124008
Jonas-we <81400544+Jonas-we@users.noreply.github.com>
2021-04-20 12:04:42 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 //https://www.eclipse.org/paho/clients/js/
-/*
+
 function Historial_1() {
 	//variabl=1;
 	console.log("Historial_1");
@@ -21,7 +21,7 @@ function Historial_2(){
     	message.destinationName = "jhsabel@gmail.com/prueba1";
     	client.send(message);
 
-}*/
+}
 
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
 
@@ -75,6 +75,9 @@ function Historial_2(){
 	boton_numero1.addEventListener("click", cambio_valor);
 	boton_numero2.addEventListener("click", cambio_valor2);
     	console.log("onMessageArrived:"+message.payloadString);
+	document.getElementById("sens1").innerHTML=message.payloadString;
+
+	  /*
 	variable=message.payloadString;
 	if(contador % 2 == 0){
 		num1 = variable ;	
@@ -88,7 +91,7 @@ function Historial_2(){
     }
 	  function cambio_valor2() {
 	  document.getElementById("sens2").innerHTML=num2;
-    }
+    }*/
   }
 
 
