54b0200dac8879e8d7b0351b51b5de81ce44c586
Jonas-we <81400544+Jonas-we@users.noreply.github.com>
2021-04-21 03:49:51 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -4,37 +4,28 @@ function Historial_1() {
 	console.log("SENSOR 1 PRENDIDO");
 	message = new Paho.MQTT.Message("Historial_1");
     	message.destinationName = "jhsabel@gmail.com/prueba1";
-    	client.send(message);
-	
+    	client.send(message);	
 }
-
-
 function Historial_2(){	
 	console.log("SENSOR 2 APAGAD");
 	message = new Paho.MQTT.Message("Historial_2");
     	message.destinationName = "jhsabel@gmail.com/prueba1";
     	client.send(message);
-
 }
 function Historial_12() {
 	console.log("SENSOR 2 PRENDIDO");
 	message = new Paho.MQTT.Message("Historial_12");
     	message.destinationName = "jhsabel@gmail.com/prueba1";
-    	client.send(message);
-	
+    	client.send(message);	
 }
-
-
 function Historial_22(){	
 	console.log("SENSOR 2 APAGADOO");
 	message = new Paho.MQTT.Message("Historial_22");
     	message.destinationName = "jhsabel@gmail.com/prueba1";
     	client.send(message);
-
 }
 */
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
-
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
@@ -49,22 +40,18 @@ function Historial_22(){
 
   // connect the client
   client.connect(options);
-   
   // called when the client connects
   function onConnect(){
-	
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
     client.subscribe("jhsabel@gmail.com/prueba1");
    message = new Paho.MQTT.Message("Conexion Establecida");
     message.destinationName = "jhsabel@gmail.com/prueba1";
     client.send(message);
-	
   }
 
   function doFail(e){
     console.log(e);
-	
   }
 
 
@@ -72,63 +59,24 @@ function Historial_22(){
   // called when the client loses its connection
   function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
-      console.log("onConnectionLost:"+responseObject.errorMessage);
-	   	    
+      console.log("onConnectionLost:"+responseObject.errorMessage); 
     }
   }
 
   // called when a message arrives
   function onMessageArrived(message,contador) {
-	//var  num1,num2 ;
-	var i ;
 	var boton_numero1 = document.getElementById("Historial_1"); //Declarar boton
-	var arr = [];
-	  function mueveReloj(){
- 	   momentoActual = new Date()
-   	 hora = momentoActual.getHours()
-   	 minuto = momentoActual.getMinutes()
-    	segundo = momentoActual.getSeconds()
-	
- 	   horaImprimible = hora + " : " + minuto + " : " + segundo
-
- 	   document.form_reloj.reloj.value = horaImprimible
-	}
 	//var boton_numero2 = document.getElementById("Historial_2");
 	boton_numero1.addEventListener("click", cambio_valor);
 	//boton_numero2.addEventListener("click", cambio_valor2);
 	
     	console.log("onMessageArrived:"+message.payloadString);
-	//document.getElementById("sens1").innerHTML=message.payloadString;
-	 // trr=message.payloadString;
-	 /* for (i = 0;i<trr.length;i++){
-	  varit.length[i]=message.payloadString;
-	  }*/
-	  File file = new File("D:\\archivos\\estudiantes.txt");
-	  
-	  for (var i = 0; i <= 10; i++) {
-  		 arr[i]=message.payloadString;
-		}
+	//document.getElementById("sens1").innerHTML=message.payloadString;  
+
 	function cambio_valor() {
 	document.getElementById("sens1").innerHTML=message.payloadString;
         document.getElementById("sens2").innerHTML=arr; 
 	  }
-	  
-
-	  /*
-	variable=message.payloadString;
-	if(contador % 2 == 0){
-		num1 = variable ;	
-	   }
-  	else
-  	{
-   		num2 = variable ;
- 	 }
-	  function cambio_valor() {
-	  document.getElementById("sens1").innerHTML=1;
-    }
-	  function cambio_valor2() {
-	  document.getElementById("sens2").innerHTML=num2;
-    }*/
   }
 
 
