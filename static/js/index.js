435ee40210938f141d03a335cb81e2dbe480562c
Jonas-we <81400544+Jonas-we@users.noreply.github.com>
2021-04-21 04:14:14 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,3 @@
-//https://www.eclipse.org/paho/clients/js/
-/*
-function Historial_1() {
-	console.log("SENSOR 1 PRENDIDO");
-	message = new Paho.MQTT.Message("Historial_1");
-    	message.destinationName = "jhsabel@gmail.com/prueba1";
-    	client.send(message);	
-}
-function Historial_2(){	
-	console.log("SENSOR 2 APAGAD");
-	message = new Paho.MQTT.Message("Historial_2");
-    	message.destinationName = "jhsabel@gmail.com/prueba1";
-    	client.send(message);
-}
-function Historial_12() {
-	console.log("SENSOR 2 PRENDIDO");
-	message = new Paho.MQTT.Message("Historial_12");
-    	message.destinationName = "jhsabel@gmail.com/prueba1";
-    	client.send(message);	
-}
-function Historial_22(){	
-	console.log("SENSOR 2 APAGADOO");
-	message = new Paho.MQTT.Message("Historial_22");
-    	message.destinationName = "jhsabel@gmail.com/prueba1";
-    	client.send(message);
-}
-*/
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
@@ -46,12 +19,11 @@ function Historial_22(){
     client.subscribe("jhsabel@gmail.com/prueba1");
     client.subscribe("jhsabel@gmail.com/prueba2");
    message = new Paho.MQTT.Message("Conexion Establecida");
-   message_2 = new Paho.MQTT.Message("Conexion Establecida");
+   message = new Paho.MQTT.Message("Conexion Establecida");
 	  
     message.destinationName = "jhsabel@gmail.com/prueba1";
-    message_2.destinationName = "jhsabel@gmail.com/prueba2";
+    message.destinationName = "jhsabel@gmail.com/prueba2";
     client.send(message);
-    client.send(message_2);
 
   }
 
@@ -69,18 +41,18 @@ function Historial_22(){
   }
 
   // called when a message arrives
-  function onMessageArrived(message,message_2) {
+  function onMessageArrived(message,message) {
 	var boton_numero1 = document.getElementById("Historial_1"); //Declarar boton
 	//var boton_numero2 = document.getElementById("Historial_2");
 	boton_numero1.addEventListener("click", cambio_valor);
 	//boton_numero2.addEventListener("click", cambio_valor2);
  	console.log("onMessageArrived:"+message.payloadString);
-	console.log("onMessageArrived_2:"+message_2.payloadString);
+	console.log("onMessageArrived_2:"+message.payloadString);
 	//document.getElementById("sens1").innerHTML=message.payloadString;  
 
 	function cambio_valor() {
 	document.getElementById("sens1").innerHTML=message.payloadString;
-        document.getElementById("sens2").innerHTML=message_2.payloadString; 
+        document.getElementById("sens2").innerHTML=message.payloadString; 
 	  }
   }
 
