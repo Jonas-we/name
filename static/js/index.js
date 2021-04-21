f54722cb0e844bc401b8f5abe170f12d47254d42
Jonas-we <81400544+Jonas-we@users.noreply.github.com>
2021-04-21 04:17:22 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -45,7 +45,6 @@
 	boton_numero1.addEventListener("click", cambio_valor);
 	//boton_numero2.addEventListener("click", cambio_valor2);
  	console.log("onMessageArrived:"+message.payloadString);
-	console.log("onMessageArrived:"+message.payloadString);
 	//document.getElementById("sens1").innerHTML=message.payloadString;  
 
 	function cambio_valor() {
